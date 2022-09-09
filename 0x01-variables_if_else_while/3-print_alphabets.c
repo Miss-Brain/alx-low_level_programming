4c35ed3fb45b9bb413b5193c099b22702840c91f
Chioma-Simon <kirasimonn@gmail.com>
2022-09-09 08:21:20 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+char l;
+char j;
+l = 'a';
+j = 'A';
+while (l <= 'z')
+{
+putchar(l);
+l++;
+
+}
+while (j <= 'Z')
+{
+putchar('\n');
+j++;
+}
+putchar('\n');
+return (0);
+}
