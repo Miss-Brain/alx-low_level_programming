0d59a2b7fadd3c71aaf8345ab5e93eb4ca661a2e
Chioma-Simon <kirasimonn@gmail.com>
2022-09-09 09:04:34 -0700

    Never thought

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char h;
+
+	h = 'a';
+	while (h <= 'z')
+{
+	if ((h != 'e') && (h != 'q'))
+{
+
+	putchar(h);
+	h++;
+}
+        else
+{
+	h++;
+}
+}
+        putchar('\n');
+	return (0);
+}
