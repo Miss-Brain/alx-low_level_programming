990e0a3b248641fdb29e5f27d0e1c34a6e15120e
Chioma-Simon <kirasimonn@gmail.com>
2022-09-09 09:40:34 -0700

    oops

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * code description
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int f = 48;
+
+	while (f < 58)
+{	
+	putchar(f);
+	f++;
+}
+	putchar('\n');
+	return (0);
+}
