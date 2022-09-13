c04388be7fa553412e7b8c2265469c76190f818c
Chioma-Simon <kirasimonn@gmail.com>
2022-09-13 00:08:42 -0700

    Putchar"

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * code description
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
