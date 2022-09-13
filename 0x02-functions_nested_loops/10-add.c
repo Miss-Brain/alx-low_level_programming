a11dff227a9d9a9d07621e151f1b493d294c7c8b
Chioma-Simon <kirasimonn@gmail.com>
2022-09-13 11:05:18 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @d: two integer arguments
+ * @b: two integer arguments
+ * Return: sum
+ */
+
+int add(int b, int d)
+{
+
+	int sum;
+
+	sum = b + d;
+
+	return (sum);
+}
