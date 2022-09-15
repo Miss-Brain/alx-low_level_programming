8f610d1c97715507f58d94baa11b1bb36d95225a
Chioma-Simon <kirasimonn@gmail.com>
2022-09-15 06:52:12 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print digit character
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char j;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		_putchar(j);
+
+	}
+	_putchar('\n');
+}
