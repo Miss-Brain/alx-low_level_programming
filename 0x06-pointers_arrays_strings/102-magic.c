c6590ff9887382c016ca8ba991a7ef204884feb6
Chioma-Simon <kirasimonn@gmail.com>
2022-09-21 13:14:44 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,6 +11,7 @@ int main(void)
 	/**
 	 * write your line of code here...
 	 * Remember:
+	 * main - magic
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
