a5becb44cefbb97a7b6cb1cc9d84e427fc09a465
Chioma-Simon <kirasimonn@gmail.com>
2022-09-26 04:59:57 -0700

    Strchr

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * strspn - return length of string that matches values consistently
+ * @s: string to search
+ * accept: target match
+ * Return: number of bytes consecutively matched
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int m, n;
+
+	/* going through string */
+
+	for (m = 0; s[m]; m++)
+	{
+		/* going through target */
+
+		for (n = 0; accept[n]; n++)
+		{
+			if (s[m] == accept[n])
+
+				break;
+		}
+
+		/* check if index don't match, return same */
+
+		if (!accept[n])
+
+			break;
+	}
+
+	/* return no. if all match till end */
+	return (m);
+}
